9cef6f5b79a713bbb96e51f407dcf143c50d4e7c
amippy <aminaka1223@gmail.com>
2015-11-10 08:32:22 +0900

    異なるデータ型で変数宣言し乗算する

--- hello.c
+++ hello.c
@@ -2,16 +2,11 @@
 
 int main(void)
 {
-	int a,b;		
+	int a = 5;
+	double b = 9.5;
 
-	a = 11;
-	b = 5;
+	printf("値は%fです。\n",a*b);
 
-	printf("11+5=%d\n",a+b);	
-	printf("11-5=%d\n",a-b);
-	printf("11x5=%d\n",a*b);
-	printf("11÷5=%d...%d\n",a/b,a%b);
-			/*この行にブレークポイントを設定*/
 
 	return 0;
 }
