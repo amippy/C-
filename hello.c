434f6648dea3009367188831526324d54e29bac1
amippy <aminaka1223@gmail.com>
2015-11-09 11:44:07 +0900

    ％fは少数出力

--- hello.c
+++ hello.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	printf("11+5=%d\n",11+5);
-	printf("11-5=%d\n",11-5);
-	printf("11X5=%d\n",11*5);
+	printf("11.0+5.0=%f\n",11.0+5.0);
+	printf("11.0-5.0=%f\n",11.0-5.0);
+	printf("11.0X5.0=%f\n",11.0*5.0);
 	printf("11.0÷5.0=%f\n",11.0/5.0);
 
 	return 0;
