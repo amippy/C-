b7a4c8f4ec3ad67cc00149167ba2f70c5420cc5d
amippy <aminaka1223@gmail.com>
2015-11-12 14:19:51 +0900

    0を入力したらループ終了しメッセを画面に表示

--- hello.c
+++ hello.c
@@ -4,12 +4,17 @@ int main(void)
 {
 	
 	int i=0;
-
-	while(i<11){
-		printf("%d\n",i);
-		i++;
-}	
 	
+	printf("do you know my favorite number? if you can answer i will kiss you lol \n");
+
+
+	do{
+		scanf("%d",&i);
+		printf("oh fuch i dont like this\n");
+	}while(i!=0);
+
+	printf("oh yes ! you gonna get my...<3\n");
+
 
 
 	return 0;
