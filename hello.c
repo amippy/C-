4d3a0296d6ca52292689a8315d08e11b9fad8b78
amippy <aminaka1223@gmail.com>
2015-11-12 16:38:09 +0900

    ４人のテストの点数を画面に表示させる

--- hello.c
+++ hello.c
@@ -3,13 +3,18 @@
 int main(void)
 {
 	
-	char i[100] ;
-
-	printf("What ur name?\n");
-	scanf("%s",i);
-	printf("hello%s!! Nice to meet u! \n",i);
-
-
+	int a,b,i[][5]={	//２次元配列の宣言
+		{90,75,70,68,93},
+		{85,72,95,78,62},
+		{100,67,86,91,76},
+		{70,92,73,100,81},
+	};
+		for (a=0;a<4;a++){
+			for(b=0;b<5;b++){
+				printf("%5d",i[a][b]);
+	}
+		printf("\n");
+	}
 
 	return 0;
 }
