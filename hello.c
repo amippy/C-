d6b19b20f6f0ea38a83aa294332a7656a69d0e68
amippy <aminaka1223@gmail.com>
2015-11-12 13:30:14 +0900

    hellodearを１０回繰り返す

--- hello.c
+++ hello.c
@@ -3,24 +3,15 @@
 int main(void)
 {
 	
-	int a;
-	
-	printf("私があなたのどこの筋肉の部位が好きか教えてあげる❤︎　1-3の数字を入力してね♪♪ウフフッ❤︎❤︎\n");
-	scanf("%d",&a);
+	int i;
 
-	switch(a){
-	case 1 :
-		printf("腹斜筋♪♪\n");
-		break;
-	
-	case 2 :
-		printf("上腕三頭金♪♪\n");
-		break;
+	for(i=0; i<=10; i=i+1){
+		printf("Hello Dear ❤︎\n");
+	}
 
-	
-	default:
-		printf("大胸筋\n");
-}
+
+
+
 
 	return 0;
 }
