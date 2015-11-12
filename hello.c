c1517a0b42b1b8853d63e518e753bb6ec6eb92a1
amippy <aminaka1223@gmail.com>
2015-11-12 12:43:53 +0900

    入力した値に応じて画面に３種類のメッセを表示

--- hello.c
+++ hello.c
@@ -8,12 +8,15 @@ int main(void)
 	printf("整数を入力して！\n");
 	scanf("%d",&a);
 
-	if(a==0){
-		printf("aの値は０です。\n");
+	if(a < 100){
+		printf("aの値は100より小さいです。\n");
 
+	}else if (a== 100){
+		printf("aの値は100と同じ値ですよ！\n");
 	}else{
-		printf("aの値は０以外ですよ！\n");
+		printf("aの値は１００より大きい値ですな\n");
 	}
+
 
 	return 0;
 }
