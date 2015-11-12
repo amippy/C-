8af06f01bef063b056deefc5bec6e2d976e932ad
amippy <aminaka1223@gmail.com>
2015-11-12 12:12:05 +0900

    実数を入力して画面に表示させる

--- hello.c
+++ hello.c
@@ -3,18 +3,13 @@
 int main(void)
 {
 	
-	int a,b;
+	double a;
 
+	printf("実数を入力してください。\n");
+	scanf("%lf",&a);
 
-	printf("足し算を行います。好きな数値（整数）を入力してください。\n");
-	printf("まずは１つ目の整数を入力してください。\n");
-	scanf("%d",&a);		//aに整数を代入
-	
-
-	printf("２つ目の整数を入力してください。\n");
-	scanf("%d",&b);		//bに整数を代入
-
-	printf("%d+%d=%d\n",a,b,a+b);
+	printf("%fを入力しました。\n",a);
 
 	return 0;
 }
+
