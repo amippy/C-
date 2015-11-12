fc1b3a1b50666c0ec331eaa39d012e9f4f407d1b
amippy <aminaka1223@gmail.com>
2015-11-12 12:32:25 +0900

    入力した値に応じて画面にメッセージを表示させる。

--- hello.c
+++ hello.c
@@ -3,13 +3,16 @@
 int main(void)
 {
 	
-	int a = 0;
+	int a;
 	
 	printf("整数を入力して！\n");
 	scanf("%d",&a);
 
-	if(a==0)
-		printf("aの値は０でーす。\n");
+	if(a >=5 && a<= 10)
+		printf("aの値は５から１０の間でーす。\n");
+
+	if(! (a >=5 && a <= 10))
+		printf("aの値は５から０の間ではないよおお！\n");
 
 	return 0;
 }
