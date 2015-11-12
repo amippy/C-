107cfda9405c7baf789f9669fb26a528154a9f6d
amippy <aminaka1223@gmail.com>
2015-11-12 12:20:42 +0900

    変数aが０の時に、画面に表示する!

--- hello.c
+++ hello.c
@@ -3,12 +3,10 @@
 int main(void)
 {
 	
-	double a;
+	int a = 0;
+	if(a== 0)		//aが０に等しい場合、処理を実行
+		printf("aの値は０だよ！！\n");
 
-	printf("実数を入力してください。\n");
-	scanf("%lf",&a);
-
-	printf("%fを入力しました。\n",a);
 
 	return 0;
 }
