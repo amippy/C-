7ff2451ebcb2256c6e98654d8bbe8c21463785ca
amippy <aminaka1223@gmail.com>
2015-11-16 12:26:13 +0900

    入力された値に応じてメッセを表示２

--- hello.c
+++ hello.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
-void keisan(int a);   //プロトタイプ宣言
+
+void keisan(int a );
 
 int main(void)
-
 {
-  printf("値はなんですか？\n");
-  keisan(10);   //keisan関数の呼び出し
+  int n;
 
+  printf("値を入力してください\n");
+  scanf("%d",&n);
+
+  keisan(n);    /*変数nを指定してkeisan関数を呼び出す*/
+ 
   return 0;
-}
 
-void keisan(int a)   // keisan関数
+}
+void keisan(int a)
 {
   if(a<=20){
-    printf("値は２０以下です。\n");
+    printf("値は20以下です。\n");
   }else{
-    printf("値は２１以上です。\n");
+    printf("値は20以上です。\n");
   }
 
-  return;
-
+  return ;
 }
+
