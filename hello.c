7e7f31aa8035a5cb647b4ff2ef43c8c5b455fa1b
amippy <aminaka1223@gmail.com>
2015-11-16 14:34:58 +0900

    変数アドレス

--- hello.c
+++ hello.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
-int a = 5;
-
-void rei(void);
 
 int main(void)
 {
-  int b=10;
+  int a =10;
 
-  printf("main関数の値\n");
-  printf("グローバル変数aの値は%d\n",a);
-  printf("ローカル変数bの値は%d\n",b);
-
-  rei();
+  printf("変数aの値は%pに格納された。\n",&a);
 
   return 0;
-}
-
-void rei(void)
-{
-  int c =20;
-
-  printf("rei関数の値\n");
-  printf("グローバル変数aの値は%d\n",a);
-  printf("ローカル変数bの値は%d\n",c);
-
-  return ;
-
 }
